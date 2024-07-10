9776aadf83930593817a404b378c21e5505ef226
kaitkt <kaitkt@inap-vision.co.jp>
2024-07-10 15:38:57 +0900

    入力した値が数値以外だった時に行うプログラムを追加

--- input.py
+++ input.py
@@ -1,4 +1,14 @@
 print('{:-^4}'.format('-じゃんけんゲームが始まるぞ！！-\n'))
 print('{:^30}'.format('入力した数だけじゃんけんできるぞ！！\n↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓\n'))
 
-input('じゃんけんしたい回数を入力してね\n--->')
+while True:
+    try:
+        n = int(input('じゃんけんしたい回数を入力してね\n--->'))
+        break
+    except ValueError:
+        pass
+    print('数字を入力してください↓↓↓↓↓↓')
+
+# for i in range(n):
+
+
