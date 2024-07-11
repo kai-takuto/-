8b904fd45e233f94eebb507ad3dcc0bd167f79a9
kaitkt <kaitkt@inap-vision.co.jp>
2024-07-11 13:22:03 +0900

    エラー文を表記させるように変更

--- zyanken.py
+++ zyanken.py
@@ -3,12 +3,13 @@ print('入力した数だけじゃんけんできるぞ！！')
 print('↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓\n')
 while True:
     try:
-        n:int = int(input('じゃんけんしたい回数を入力してね--->'))
+        n:int = int(input('じゃんけんしたい回数(自然数)を入力してね--->'))
         if n <= 0:
             raise ValueError
         break
     except ValueError:
         pass
+    print('＊＊エラー：入力した値は自然数ではありません＊＊')
     print('自然数(1, 2, 3...)を入力してください↓↓↓↓↓↓')
 
 # for i in range(n):
