741621bd3e20e110728478d94df0ee1f7b77d548
kaitkt <kaitkt@inap-vision.co.jp>
2024-07-16 10:45:42 +0900

    じゃんけんの結果を記録して、リストとして表示させるようにした

--- zyanken.py
+++ zyanken.py
@@ -1,8 +1,5 @@
-print('-じゃんけんゲームが始まるぞ！！-\n')
-print('入力した数だけじゃんけんできるぞ！！')
-print('↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓\n')
-#(グー：１,チョキ：２,パー：３)のどれかを入力させて出し手を比較
-def zyanken(your_hands:int, cpu_hands:int):
+
+def zyanken(your_hands: int, cpu_hands: int) -> str:
     if your_hands == cpu_hands:
         print('同じ手を出した！結果はあいこ！')
         return 'draw'
@@ -11,10 +8,10 @@ def zyanken(your_hands:int, cpu_hands:int):
         return 'win'
     elif your_hands == 1 and cpu_hands == 3:
         print('(あなた)はグー、(相手)はパーを出した！あなたの負け！')
-        return 'loss'
+        return 'lose'
     elif your_hands == 2 and cpu_hands == 1:
         print('(あなた)はチョキ、(相手)はグーを出した！あなたの負け！')
-        return 'loss'
+        return 'lose'
     elif your_hands == 2 and cpu_hands == 3:
         print('(あなた)はチョキ、(相手)はパーを出した！あなたの勝ち！')
         return 'win'
@@ -23,41 +20,44 @@ def zyanken(your_hands:int, cpu_hands:int):
         return 'win'
     elif your_hands == 3 and cpu_hands == 2:
         print('(あなた)はパー、(相手)はチョキを出した！あなたの負け！')
-        return 'loss'
+        return 'lose'
     else:
         print('＊＊エラー：1~3の数字を入力してください＊＊')
 
-def print_janken_results(results):
+
+def zyanken_results(results: list):
     wins = results.count('win')
-    losses = results.count('loss')
+    lose = results.count('lose')
     draws = results.count('draw')
-    if results == 'win':
-        win += 1
-    elif results == 'loss':
-        losse += 1
-    elif results == 'draw':
-        draw += 1
-    
+
     print(f"勝利数: {wins}")
-    print(f"敗北数: {losses}")
+    print(f"敗北数: {lose}")
     print(f"引き分け数: {draws}")
-    
-results = []
 
-while True:
-    try:
-        n:int = int(input('じゃんけんしたい回数(自然数)を入力してね--->'))
-        if n > 0:
-            break
-        else:
-            raise ValueError('input value is not valid integer. it must be number that larger than 0')
-    except ValueError as e:
-        print(e)
-        print('＊＊エラー：入力した値は自然数ではありません＊＊')
-        print('自然数(1, 2, 3...)を入力してください↓↓↓↓↓↓')
 
-        
+def main():
+    print('-じゃんけんゲームが始まるぞ！！-\n')
+    print('入力した数だけじゃんけんできるぞ！！')
+    print('↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓\n')
+    while True:
+        try:
+            n: int = int(input('じゃんけんしたい回数(自然数)を入力してね--->'))
+            if n > 0:
+                break
+            else:
+                raise ValueError('input value is not valid integer. it must be number that larger than 0')
+        except ValueError as e:
+            print(e)
+            print('＊＊エラー：入力した値は自然数ではありません＊＊')
+            print('自然数(1, 2, 3...)を入力してください↓↓↓↓↓↓')
 
-# for i in range(n):
+
+if __name__ == "__main__":
+    print_results = []
+    result = zyanken(your_hands=3, cpu_hands=3)
+    print_results.append(result)
+    zyanken_results(print_results)
+
+
     
         
